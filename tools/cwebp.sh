75fbee7d2027e4dd154d8042683630195c4584a6
Suleman Hasib <suleman.hasib@gmail.com>
2025-01-20 14:56:22 +0000

    modify cwebp script to conditionally remove original images only if not in WebP format

--- tools/cwebp.sh
+++ tools/cwebp.sh
@@ -4,4 +4,6 @@ input_file="$1"
 output_file="${input_file%.*}.webp"
 image_dir="assets/img/"
 cwebp -q 75 "${image_dir}${input_file}" -o "${image_dir}${output_file}"
-rm "${image_dir}${input_file}"
+if [[ "${input_file##*.}" != "webp" ]]; then
+  rm "${image_dir}${input_file}"
+fi
